e45d791396b4aff3ac511aba693b61dbf022f0d1
unknown <jose.filipe.toledo.1999@gmail.com>
2022-08-25 23:52:14 -0300

    fiz uns gets uns posts

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 import express from 'express';
+import cors from 'cors';
 
-const app = express(); // cria um servidor
+const app = express(); 
+app.use(cors());
 app.use(express.json());
 
+
 const users = [];
+const tweets = [];
 
 app.post('/sign-up', (req, res) => 
 {
-
+  console.log("logou");
   const user = {id : users.length + 1,
-                username : req.body.email, 
-                avatar : req.body.password};
+                username : req.body.username, 
+                avatar : req.body.avatar};
   users.push(user);
-  res.send("ok");
+  res.send("OK");
+});
+
+app.post('/tweets', (req, res) => 
+{
+  
+  const user = users.find(element => element.username === req.body.username);
+
+  console.log(user);
+
+  const tweet = {id : tweets.length + 1,
+                username : user.username,
+                avatar : user.avatar, 
+                tweet : req.body.tweet}; 
+  tweets.push(tweet);
+  res.send("OK");
 });
 
 app.get('/tweets', (req, res) => 
 {
-  res.send(users);
+
+  res.send(tweets);
 });
 
 app.listen(5000);
